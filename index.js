418418682341dae9507068c832d7fa69161f0e98
AmosesYT <tinyaidan01@gmail.com>
2024-09-29 01:41:43 -0700

    Optimizations

--- index.js
+++ index.js
@@ -2,69 +2,52 @@ const toDoButton = document.getElementById('toDoButton')
 const toDoContainer = document.getElementById('toDoContainer');
 const inputField = document.getElementById('inputField');
 
-
-toDoButton.addEventListener('click', function () {
+function doThing() {
     if (inputField.value == "") {
         console.log("error")
     } else {
-        var paragraph = document.createElement('li')
-        var button = document.createElement('button')
-        var newInput = document.createElement('input')
-        newInput.type = "checkbox"
-        newInput.checked = false;
-        button.classList.add('custom-button')
-        paragraph.classList.add('paragraph-styling')
-        newInput.classList.add('newInput')
-        paragraph.innerText = inputField.value.toUpperCase();
-        button.innerText = 'X'
-        toDoContainer.appendChild(paragraph)
-        paragraph.appendChild(newInput)
-        paragraph.appendChild(button)
-        inputField.value = "";
-    }
-    button.addEventListener('click', function () {
-        toDoContainer.removeChild(paragraph)
-    })
-   newInput.addEventListener('change',function() {
-    if(this.checked) {
-        paragraph.style.textDecoration = 'line-through'
-    } else {
-        paragraph.style.textDecoration = 'none'
-    }
-   })
-/*
-    if (newInput.checked != true) {
-        paragraph.style.textDecoration = none;
-    } else if (newInput.checked = true) {
-        paragraph.style.textDecoration = 'line-through'
-    }
+        // Check if your full, if you arent, then do all this biz:
+        
+            var paragraph = document.createElement('li')
+            var button = document.createElement('button')
+            var newInput = document.createElement('input')
+            var textElement = document.createElement('span');
+            newInput.type = "checkbox"
+            newInput.checked = false;
+            button.classList.add('custom-button')
+            paragraph.classList.add('paragraph-styling')
+            newInput.classList.add('newInput')
+            textElement.innerText = inputField.value.toUpperCase();
+            button.innerText = 'X'
+            toDoContainer.appendChild(paragraph)
+            paragraph.appendChild(newInput) // Checkbox
+            paragraph.appendChild(textElement) // Text
+            paragraph.appendChild(button) // X button
+            inputField.value = "";
+        
+            button.addEventListener('click', function () {
+                toDoContainer.removeChild(paragraph)
+            })
+        
+            newInput.addEventListener('change', () => {
+                if (newInput.checked) {
+                    textElement.style.textDecoration = 'line-through'
+                } else {
+                    textElement.style.textDecoration = 'none'
+                }
+            })
+        
 
-    */
+    }
+}
+ 
+toDoButton.addEventListener('click', function () {
+    doThing();
 })
 
-
-
-
-
- /*inputField.addEventListener('keypress', function (event) {
+inputField.addEventListener('keypress', (event) => {
     if (event.key === "Enter") {
-        var paragraph = document.createElement('li')
-        var button = document.createElement('button')
-        button.classList.add('custom-button')
-        paragraph.classList.add('paragraph-styling')
-        paragraph.innerText = inputField.value.toUpperCase();
-        button.innerText = 'X'
-        toDoContainer.appendChild(paragraph)
-        paragraph.appendChild(button)
-        inputField.value = "";
+        doThing();
     }
-    button.addEventListener('click', function () {
-        toDoContainer.removeChild(paragraph)
-    })
-})
-
-*/
-
-
-
+});
 
